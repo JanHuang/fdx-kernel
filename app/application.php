2081bd20a626e1048bbbfe4b8869bdfc06ec2e7d
janhuang <384099566@qq.com>
2016-06-12 23:18:21 +0800

    add test

---
+++ app/application.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+class Application
+{
+    public function getHandle()
+    {
+
+    }
+
+    public function getServer()
+    {
+
+    }
+
+    public function getMonitor()
+    {
+        
+    }
+}
